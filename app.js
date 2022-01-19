d188427d9fac101a67083850abdabcd6a8d8ef1f
Deborah Campos <631care@gmail.com>
2022-01-19 16:31:55 -0500

    put, post, delete functional

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.get("/transac", (req,res)=>{
 });
 app.get("/transac/:index", (req,res)=>{
     const {index} = req.params;
-
     if(transac[index]){
         res.json(transac[index])
     }else{
@@ -25,6 +24,41 @@ app.get("/transac/:index", (req,res)=>{
 app.get("*", (req, res)=>{
     res.status(404).json({error: "Page not found"});
 });
+
+app.post("/transac", (req,res)=>{
+    transac.push(req.body)
+    res.json(transac[transac.length-1])
+})
+app.put("/transac/:index", (req,res)=>{
+    const {index} = req.params;
+    if(!transac[index]){
+        res.redirect("*")
+    }
+    const {date, name, amount, from} = req.body;
+    if(isNaN(amount)){
+        res.status(422).json({
+            error: "Please enter a numeric value"
+        });
+    }
+    if(date && name && (typeof amount === "number") && from){
+        transac[index] = {date, name, amount, from};
+        res.json(transac[index])
+    } else {
+        res.status(422).json({
+            error: "Please provide all fields"
+        });
+    }
+});
+app.delete("/transac/:index", (req, res)=>{
+    const{index} = req.params;
+    if(transac[index]){
+        let removed = transac.splice(index,1)
+        res.json(removed)
+    } else {
+        res.redirect("*")
+    }
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
