3aaa6a1193348a7ff14b528ffa5d5b7ea3d5984c
Deborah Campos <631care@gmail.com>
2022-01-19 14:53:54 -0500

    get requests & API functional

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const app = express();
+const transac = require("./models/transactions.js");
 require("dotenv").config();
 const PORT = process.env.PORT;
 const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Welcome to our bookmarks app<h1>")
+    res.send("<h1>Welcome to the Budget App <h1>")
 });
+app.get("/transac", (req,res)=>{
+    res.json(transac)
+});
+app.get("/transac/:index", (req,res)=>{
+    const {index} = req.params;
+
+    if(transac[index]){
+        res.json(transac[index])
+    }else{
+        res.redirect("*")
+    }
+})
 app.get("*", (req, res)=>{
     res.status(404).json({error: "Page not found"});
 });
-
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
---
+++ models/transactions.js
@@ -0,0 +1,69 @@
+module.exports = [
+    {
+      date: "December 1",
+      name: "Groceries",
+      amount: -65,
+      from: "ShopRite"
+    },
+    {
+      date: "December 1",
+      name: "Shopping",
+      amount: -57,
+      from: "Lowes"
+    },
+    {
+      date: "December 2",
+      name: "Direct Deposit",
+      amount: 1000,
+      from: "Coinbase"
+    },
+    {
+      date: "December 3",
+      name: "Gas",
+      amount: -35,
+      from: "Shell Gas Station"
+    },
+    {
+      date: "December 9",
+      name: "Income",
+      amount: 1000,
+      from: "Coinbased"
+    },
+    {
+      date: "December 9",
+      name: "Groceries",
+      amount: -80,
+      from: "King Kullen"
+    },
+    {
+        date: "December 15",
+        name: "Rent",
+        amount: -1700,
+        from: "Fairfield"
+    },
+    {
+        date: "December 16",
+        name: "Income",
+        amount: 1000,
+        from: "Coinbased"
+    },
+    {
+        date: "December 16",
+        name: "Pharmacy",
+        amount: -40,
+        from: "Walgreens"
+    },
+    {
+        date: "December 20",
+        name: "Utilites - Gas",
+        amount: -45,
+        from: "National Grid"
+    },
+    {
+        date: "December 20",
+        name: "Utilites - Electric",
+        amount: -55,
+        from: "National Grid"
+    },
+
+  ];
